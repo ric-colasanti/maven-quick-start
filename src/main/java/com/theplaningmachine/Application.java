1c3cab19263e25a9ef2a3a99a1334fb2fa8e3851
Dr Ric Colasanti <ricardo.colasanti@glasgow.ac.uk>
2024-05-15 15:08:31 +0100

    UseStringUtils and add dependancy to pom

--- src/main/java/com/theplaningmachine/Application.java
+++ src/main/java/com/theplaningmachine/Application.java
@@ -25,9 +25,15 @@ package com.theplaningmachine;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
     
+    public int countWords(String words){
+        String[] separateWords = StringUtils.split(words,' ');
+        return (separateWords == null)? 0: separateWords.length;
+    }
+
     public void greet(){
         List<String> greetings = new ArrayList<>();
         greetings.add("Hello");
@@ -42,8 +48,11 @@ public class Application {
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
-    	System.out.println ("Starting Application");
-	Application app = new Application();
-    app.greet();
+    	System.out.println ("Starting Application2");
+	    Application app = new Application();
+        app.greet();  
+        int count = app.countWords("I have four words");
+        System.out.println(count);
+      
     }
 }
